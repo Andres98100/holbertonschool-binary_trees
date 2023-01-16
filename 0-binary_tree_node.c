260e9ac1162c54a4174ca1b1d4bf5610cac85901
andres98100 <andressolarte580@gmail.com>
2023-01-16 16:46:12 -0500

    create binary tree

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - function create a binary tree
+ * @parent: variable struct
+ * @value: variable int
+ * Return: struct
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(*parent));
+	if (!new)
+		return (NULL);
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+	return (new);
+}
---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - function  inserts a node at left
+ * @parent: variable struct
+ * @value: variable int
+ * Return: struct
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(*parent));
+	if (!new || !parent)
+		return (NULL);
+	new = binary_tree_node(parent, value);
+	if (parent->left)
+	{
+		new->left = parent->left;
+		new->left->parent = new;
+	}
+	parent->left = new;
+
+	return (new);
+}
