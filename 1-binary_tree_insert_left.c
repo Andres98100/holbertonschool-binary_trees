9beb357fd82240ad3c2b525f0f348964e4a497ba
andres98100 <andressolarte580@gmail.com>
2023-01-16 16:56:23 -0500

    update solution insert node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 	
-	if (!new || !parent)
+	if (!parent)
 		return (NULL);
 	new = binary_tree_node(parent, value);
 	if (parent->left)
