a49942f9f8e022a8ea8fe55fa6afbe5879652b15
andres98100 <andressolarte580@gmail.com>
2023-01-17 11:11:29 -0500

    update pre, in and post

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -11,7 +11,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree && func)
 	{
 		binary_tree_inorder(tree->left, func);
-		func(tree->n)
+		func(tree->n);
 		binary_tree_inorder(tree->right, func);
 	}
 }
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -12,6 +12,6 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	{
 		binary_tree_postorder(tree->left, func);
 		binary_tree_postorder(tree->right, func);
-		func(tree->n)
+		func(tree->n);
 	}
 }
