e5630d4c1c8f0d72b026b4b42f4a836e38dd7af9
jegomezV <jegomezvalencia@gmail.com>
2023-01-18 14:47:43 -0500

    final push

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,11 +1,54 @@
 #include "binary_trees.h"
-/**
- * binary_tree_balance - function measures the balance factor of a binary tree
- * @tree: variable struct
- * Return: int
- */
 
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ *
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ *
+ * Return: Balance factor. 0 if tree is NULL
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-   
+	int h_left = 1;
+	int h_right = 1;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left)
+		h_left += height(tree->left);
+	else
+		h_left = 0;
+	if (tree->right)
+		h_right += height(tree->right);
+	else
+		h_right = 0;
+	return (h_left - h_right);
+}
+
+/**
+ * height - gets the height of a tree
+ *
+ * @tree: tree to get the height from
+ *
+ * Return: height of the tree
+ */
+int height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (-1);
+	else
+		return (1 + max(height(tree->left), height(tree->right)));
+}
+
+/**
+ * max - get the max number between two
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: ax between a and b
+ */
+int max(int a, int b)
+{
+	return ((a >= b) ? a : b);
 }
