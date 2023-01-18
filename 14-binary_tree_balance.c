bab0f552838d23b2cf9317bfa4aae2bb49afcbe4
andres98100 <andressolarte580@gmail.com>
2023-01-18 14:55:43 -0500

    update

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,5 +7,4 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-   
 }
---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - function checks if a binary tree is perfect
+ * @tree: variable struct
+ * Return: int
+*/
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+
+}
