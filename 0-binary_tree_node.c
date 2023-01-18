6534d42499c8c31323e0e5154b4d3ec5618f6d51
andres98100 <andressolarte580@gmail.com>
2023-01-18 10:13:25 -0500

    update solution error

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	new = malloc(sizeof(*parent));
+	new = malloc(sizeof(binary_tree_t));
 	if (!new)
 		return (NULL);
 	new->n = value;
